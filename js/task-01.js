9e0fde60d78e5620d2206140d674dedc57809530
lhoydjuanillo <lhoydjuani@gmail.com>
2023-03-22 23:18:03 +0800

    first

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+const categoriesNumberEl = document.querySelectorAll('h2');
+const catNumber = categoriesNumberEl.length;
+
+//  item.forEach(element => {
+//     const categoriesElms = element.querySelectorAll('li');
+// });
+// console.log(item);
+
+console.log(`Number of Categories: ${catNumber}`);
+console.log("");
+console.log(`Category: ${categoriesNumberEl[0].innerHTML}`);
+console.log(`Elements: `);
+
+
+
